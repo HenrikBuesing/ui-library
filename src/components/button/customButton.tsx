db6b334d3ce829b6032242b0dcefe850165452a7
HBuesing <buesing.henrik@t-online.de>
2024-06-21 17:01:34 +0200

    *refactors button component

--- src/components/button/customButton.tsx
+++ src/components/button/customButton.tsx
@@ -2,43 +2,40 @@ import React, {ComponentPropsWithoutRef, CSSProperties} from 'react';
 import './button.scss';
 
 interface ICustomButton extends ComponentPropsWithoutRef<'button'> {
-  label: string
-  theme?: string
-  small?: boolean
-  disabled?: boolean
+  label    : string;
+  disabled?: boolean;
+  small?   : boolean;
+  theme?   : string;
 }
-
 
 export function CustomButton(props: ICustomButton) {
   const {
-    label,
-    theme,
-    small = true,
     disabled = false,
+    label,
+    small = false,
+    theme,
     ...buttonProps
   } = props;
 
-
-  function getColor(bgColor: string) {
+  function setColor(bgColor: string) {
     if (!theme || disabled) return null;
 
-    let color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
-    let r = parseInt(color.substring(0, 2), 16); // hexToR
-    let g = parseInt(color.substring(2, 4), 16); // hexToG
-    let b = parseInt(color.substring(4, 6), 16); // hexToB
-    let uicolors = [r / 255, g / 255, b / 255];
-    let c = uicolors.map((col) => {
-      if (col <= 0.03928) {
-        return col / 12.92;
-      }
-      return Math.pow((col + 0.055) / 1.055, 2.4);
+    const color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+    const uiColors = [
+      parseInt(color.substring(0, 2), 16) / 255,
+      parseInt(color.substring(2, 4), 16) / 255,
+      parseInt(color.substring(4, 6), 16) / 255
+    ];
+    const c = uiColors.map(col => {
+      return col <= 0.03928? (col / 12.92) : Math.pow((col + 0.055) / 1.055, 2.4);
     });
-    let L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+    const L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+
     return L > 0.179 ? '#000000' : '#ffffff';
   }
 
   const style: CSSProperties = {
-    color: getColor(theme),
+    color: setColor(theme),
     backgroundColor: disabled? null : theme,
     fontSize: small? '1rem' : null,
     lineHeight: small? '1rem' : null,
@@ -47,7 +44,7 @@ export function CustomButton(props: ICustomButton) {
   }
 
   return (
-    <button className={`uil-button ${disabled? 'uil-disabled' : ''}`} style={style} disabled={disabled} {...buttonProps}>
+    <button className={`uil-button ${disabled && 'uil-disabled'}`} style={style} disabled={disabled} {...buttonProps}>
       {label}
     </button>
   );
