46cb221e0bc1936613aa56a03e1ed4efa2c3017b
HBuesing <hbuesing@newyorker.de>
2024-07-05 10:31:32 +0200

    removes button showcase component

--- website/src/theme/MDXComponents/index.ts
+++ website/src/theme/MDXComponents/index.ts
@@ -1,6 +1,9 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import AssignableProps from '@site/src/components/Props';
+import {CustomButton} from '../../../../packages/dist/index';
+
 export default {
   ...MDXComponents,
   AssignableProps,
+  CustomButton,
 };
