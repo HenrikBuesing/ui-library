5e751c1d29799cda3e796db9382fa855edca4b58
HBuesing <hbuesing@newyorker.de>
2024-07-15 15:47:40 +0200

    *adds modal docs

---
+++ website/src/components/modal/index.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { NotifyModal, QuestionModal, CustomButton } from '../../uil-bundle/ui-library.modern.mjs';
+
+export default function SuccessModal() {
+  const [modalVis, setModalVis] = useState(false);
+
+  return (
+    <>
+      <CustomButton label={'show success'} theme={'#006A4E'} onClick={() => setModalVis(true)} />
+
+      {modalVis &&
+        <NotifyModal title={'Success'} message={'This is a success message'} close={() => setModalVis(false)} closeLabel={'close'} modalType={'success'} />
+      }
+    </>
+  );
+}
+
+export function WarningModal() {
+  const [modalVis, setModalVis] = useState(false);
+
+  return (
+    <>
+      <CustomButton label={'show warning'} theme={'#FFD700'} onClick={() => setModalVis(true)} />
+
+      {modalVis &&
+        <NotifyModal title={'Warning'} message={'This is a warning message'} close={() => setModalVis(false)} closeLabel={'close'} modalType={'warning'} />
+      }
+    </>
+  );
+}
+
+export function ErrorModal() {
+  const [modalVis, setModalVis] = useState(false);
+
+  return (
+    <>
+      <CustomButton label={'show error'} theme={'#800020'} onClick={() => setModalVis(true)} />
+
+      {modalVis &&
+        <NotifyModal title={'Error'} message={'This is an error'} close={() => setModalVis(false)} closeLabel={'close'} modalType={'error'} />
+      }
+    </>
+  );
+}
+
+export function MultiLineModal() {
+  const [modalVis, setModalVis] = useState(false);
+
+  return (
+    <>
+      <CustomButton label={'show modal'} onClick={() => setModalVis(true)} />
+
+      {modalVis &&
+        <NotifyModal title={'Multiple messages'} message={['first string', 'this is a second text', 'Each string is wrapped in a paragraph']} close={() => setModalVis(false)} closeLabel={'close'} modalType={'success'} />
+      }
+    </>
+  );
+}
+
+export function CallbackModal() {
+  const [modalVis, setModalVis] = useState(false);
+
+  function alertTimeout() {
+    alert('3 second timeout alert');
+  }
+
+  return (
+    <div style={{marginBottom: '2rem'}}>
+      <CustomButton label={'show timeout modal'} onClick={() => setModalVis(true)} />
+
+      {modalVis &&
+        <NotifyModal title={'Multiple messages'} message={'Display an alert after 3 second timeout'} close={() => setModalVis(false)} closeLabel={'close'} modalType={'success'} timeout={3000} callback={alertTimeout}/>
+      }
+    </div>
+  );
+}
+
+export function QuestionExample() {
+  const [modalVis, setModalVis] = useState(false);
+
+  return (
+    <>
+      <CustomButton label={'show question modal'} onClick={() => setModalVis(true)} />
+
+      {modalVis &&
+        <QuestionModal
+          title={'Question title'}
+          message={['Confirm to display an alert', 'cancel to close this modal']}
+          cancel={() => setModalVis(false)}
+          cancelLabel={'cancel'}
+          confirm={() => alert('Hello!')}
+          confirmLabel={'confirm'}
+        />
+      }
+    </>
+  );
+}
--- website/src/theme/MDXComponents/index.ts
+++ website/src/theme/MDXComponents/index.ts
@@ -1,6 +1,7 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import AssignableProps from '@site/src/components/Props';
 import DefaultInput , {TooltipInput, IconInput, DisabledInput} from '@site/src/components/Input';
+import SuccessModal, {ErrorModal, WarningModal, MultiLineModal, CallbackModal, QuestionExample} from '@site/src/components/modal';
 import {CustomButton, CustomInput, useClickOutsideRef} from '../../uil-bundle/ui-library.modern.mjs';
 
 export default {
@@ -12,5 +13,11 @@ export default {
   DisabledInput,
   CustomButton,
   CustomInput,
+  SuccessModal,
+  WarningModal,
+  ErrorModal,
+  MultiLineModal,
+  CallbackModal,
+  QuestionExample,
   useClickOutsideRef,
 };
