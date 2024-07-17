9700873f073ea2b0cf1b2c0abacf819024d614ae
HBuesing <hbuesing@newyorker.de>
2024-07-17 11:50:53 +0200

    *refactors component styling

--- website/src/uil-bundle/index.d.ts
+++ website/src/uil-bundle/index.d.ts
@@ -3,5 +3,5 @@ export { CustomCheckBox } from './components/checkbox/index';
 export { SVG } from './components/images/index';
 export { CustomInput, PasswordInput, PasswordRule } from './components/input/index';
 export { NotifyModal, QuestionModal } from './components/modal/index';
-export { useClickOutsideRef } from './hooks/index';
+export { useClickOutsideRef, useGetColor } from './hooks/index';
 export { PasswordRuleTypes } from './enums/index';
