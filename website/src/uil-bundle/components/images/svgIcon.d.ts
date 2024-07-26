92ea3fa6feeaf495477ca985edc5b55c7e041fcb
HBuesing <buesing.henrik@t-online.de>
2024-07-26 20:44:50 +0200

    *moves uil-bundle to gitignore

--- website/src/uil-bundle/components/images/svgIcon.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { ComponentPropsWithoutRef } from 'react';
-interface ISvgIcon extends ComponentPropsWithoutRef<'svg'> {
-    src: string;
-    color?: string | undefined;
-    height?: number;
-    width?: number;
-}
-export declare function SVG(props: ISvgIcon): React.JSX.Element;
-export {};
--- website/src/uil-bundle/hooks/getColor.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function useGetColor(backgroundColor: string): "#000000" | "#ffffff";
