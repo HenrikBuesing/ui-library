124718d4fb9ebc53c56324e449c5949fe82e2e69
HBuesing <buesing.henrik@t-online.de>
2024-09-21 13:54:13 +0200

    *updates radio docs

--- website/src/components/check/index.tsx
+++ website/src/components/check/index.tsx
@@ -2,14 +2,14 @@ import React, {useState} from 'react';
 import {CustomRadio, CustomCheckbox} from "../../uil-bundle/bundle.js";
 import {RadioOption} from "../../uil-bundle";
 
-const options: RadioOption[] = [
-  {value: 'option-a', label: 'option-a'},
-  {value: 'option-b', label: 'option-b'},
-  {value: 'option-c', label: 'option-c'},
-  {value: 'option-d', label: 'option-d', disabled: true},
-];
-
 export default function RadioExample() {
+  const options: RadioOption[] = [
+    {value: 'option-a', label: 'option-a', checked: true},
+    {value: 'option-b', label: 'option-b'},
+    {value: 'option-c', label: 'option-c'},
+    {value: 'option-d', label: 'option-d', disabled: true},
+  ];
+
   const [value, setValue] = useState<string>('');
 
   return (
@@ -18,26 +18,47 @@ export default function RadioExample() {
 }
 
 export function RadioLabel() {
+  const optionsB: RadioOption[] = [
+    {value: 'option-e', label: 'option-e'},
+    {value: 'option-f', label: 'option-f'},
+    {value: 'option-g', label: 'option-g'},
+    {value: 'option-h', label: 'option-h', disabled: true},
+  ];
+
   const [value, setValue] = useState<string>('');
 
   return (
-    <CustomRadio options={options} value={value} valueChanged={setValue} label={'Label property'}/>
+    <CustomRadio options={optionsB} value={value} valueChanged={setValue} label={'Label property'}/>
   );
 }
 
 export function RadioColor() {
+  const optionsC: RadioOption[] = [
+    {value: 'option-i', label: 'option-i'},
+    {value: 'option-j', label: 'option-j'},
+    {value: 'option-k', label: 'option-k'},
+    {value: 'option-l', label: 'option-l', disabled: true},
+  ];
+
   const [value, setValue] = useState<string>('');
 
   return (
-    <CustomRadio options={options} value={value} valueChanged={setValue} checkColor={'red'}/>
+    <CustomRadio options={optionsC} value={value} valueChanged={setValue} checkColor={'red'}/>
   );
 }
 
 export function RadioDisabled() {
+  const optionsD: RadioOption[] = [
+    {value: 'option-m', label: 'option-m'},
+    {value: 'option-n', label: 'option-n'},
+    {value: 'option-o', label: 'option-o'},
+    {value: 'option-p', label: 'option-p', disabled: true},
+  ];
+
   const [value, setValue] = useState<string>('');
 
   return (
-    <CustomRadio options={options} value={value} valueChanged={setValue} disabled={true}/>
+    <CustomRadio options={optionsD} value={value} valueChanged={setValue} disabled={true}/>
   );
 }
 
