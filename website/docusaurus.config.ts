647fb93adbd6e631f67eae5ea6a37ba7f9ca8a42
HBuesing <hbuesing@newyorker.de>
2024-10-25 14:54:58 +0200

    *reverts assigning new url to docs

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   title: 'UI Library Docs',
   favicon: 'img/favicon.ico',
 
-  url: 'https://www.ui-library.docs.hbsng.com',
+  url: 'https://hb-ui-library-docs.vercel.app',
   baseUrl: '/',
   trailingSlash: false,
   i18n: {
