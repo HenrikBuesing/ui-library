c219cf1c2a3c1b332b600a06940d71e69994164a
HBuesing <hbuesing@newyorker.de>
2024-11-22 08:50:15 +0100

    *removes overriding typescript-eslint rules

--- packages/src/globals.d.ts
+++ packages/src/globals.d.ts
@@ -1,2 +1,9 @@
-declare module "*.module.css";
-declare module "*.module.scss";
+declare module '*.module.css' {
+  const classes: Record<string, string>;
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: Record<string, string>;
+  export default classes;
+}
