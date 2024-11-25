9664f4872a3300945b16ebbf407b8af71cf93c45
HBuesing <hbuesing@newyorker.de>
2024-11-25 14:04:07 +0100

    * restructures checkbox type definition

---
+++ packages/src/components/check/checkbox/types.ts
@@ -0,0 +1,9 @@
+import type {BaseProps, Label} from '@common/types';
+
+export type CheckboxProps = BaseProps<'input'> & AdditionalCheckboxProps;
+
+type AdditionalCheckboxProps = Label & {
+  checked: boolean;
+  toggleCheck: (value: boolean) => void;
+  color?: string;
+}
