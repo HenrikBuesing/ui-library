70724ce01f7f445bc3c1e4acf54b055e7db2cde4
HBuesing <hbuesing@newyorker.de>
2024-11-29 13:58:30 +0100

    * adds checkbox tests

--- packages/src/components/check/checkbox/checkbox.test.tsx
+++ packages/src/components/check/checkbox/checkbox.test.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import {describe, expect, test} from 'vitest';
 import {Checkbox} from "./Checkbox";
 
@@ -19,13 +19,43 @@ describe('general', () => {
     expect(child).toBeDefined();
     expect(child.textContent).toEqual('Hello world');
   });
+
+  test('should render checkbox using custom color', () => {
+    const {container} = render(<ColorCheckbox/>);
+
+    const checkmark = container.querySelector('.checkmark') as HTMLDivElement;
+
+    expect(checkmark.style.backgroundColor).toBeDefined();
+    expect(checkmark.style.backgroundColor).toEqual('red');
+  });
+
+  test('should check checkbox', () => {
+    const {container} = render(<BasicCheckbox/>);
+
+    const checkbox = container.querySelector('.checkbox') as HTMLDivElement;
+
+    fireEvent.click(checkbox);
+
+    expect(screen.getByRole('checkbox').checked).toBeTruthy();
+  });
 });
 
 function BasicCheckbox() {
   const [checked, setChecked] = useState(false);
 
   return (
-    <Checkbox checked={checked} toggleCheck={setChecked} label={'checkbox'}/>
+    <>
+      <div>{checked}</div>
+      <Checkbox checked={checked} toggleCheck={setChecked} label={'checkbox'}/>
+    </>
+  );
+}
+
+function ColorCheckbox() {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <Checkbox checked={checked} toggleCheck={setChecked} label={'checkbox'} color={'red'}/>
   );
 }
 
