c00043e7b72fa137ade325cd9aad993a57077bac
HBuesing <hbuesing@newyorker.de>
2024-12-02 09:57:37 +0100

    * adds checkbox tests (dark mode & disabled)

--- packages/src/components/check/checkbox/checkbox.test.tsx
+++ packages/src/components/check/checkbox/checkbox.test.tsx
@@ -29,24 +29,51 @@ describe('general', () => {
     expect(checkmark.style.backgroundColor).toEqual('red');
   });
 
-  test('should check checkbox', () => {
-    const {container} = render(<BasicCheckbox/>);
+  test('should render checkbox using dark mode', () => {
+    const {container} = render(<BasicCheckbox dark={true}/>);
 
+    const checkbox = container.querySelector('.checkbox') as HTMLDivElement;
+    const label = screen.getByText('checkbox') as HTMLLabelElement;
+
+    expect(checkbox.className).toMatch(/\bdark\b/);
+    expect(label.className).toMatch(/\bdark\b/);
+  });
+
+  test('should render disabled checkbox', () => {
+    const {container} = render(<BasicCheckbox disabled={true}/>);
+
+    const input = screen.getByRole('checkbox') as HTMLInputElement;
     const checkbox = container.querySelector('.checkbox') as HTMLDivElement;
 
     fireEvent.click(checkbox);
 
-    expect(screen.getByRole('checkbox').checked).toBeTruthy();
+    expect(input.disabled).toBeTruthy();
+    expect(input.checked).toBeFalsy();
+  });
+
+  test('should check checkbox', () => {
+    const {container} = render(<BasicCheckbox />);
+
+    const input = screen.getByRole('checkbox') as HTMLInputElement;
+    const checkbox = container.querySelector('.checkbox') as HTMLDivElement;
+
+    // check with wrapper div
+    fireEvent.click(checkbox);
+    expect(input.checked).toBeTruthy();
+    
+    // uncheck with input (is visually hidden but should technically still work)
+    fireEvent.click(input);
+    expect(input.checked).toBeFalsy();
   });
 });
 
-function BasicCheckbox() {
+function BasicCheckbox({dark, disabled} :{dark?: boolean, disabled?: boolean}) {
   const [checked, setChecked] = useState(false);
 
   return (
     <>
       <div>{checked}</div>
-      <Checkbox checked={checked} toggleCheck={setChecked} label={'checkbox'}/>
+      <Checkbox checked={checked} toggleCheck={setChecked} label={'checkbox'} dark={dark ?? false} disabled={disabled ?? false} />
     </>
   );
 }
