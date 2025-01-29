d27ef257ef7985b61ac02b0d5c7db5e6b05f24e7
HBuesing <hbuesing@newyorker.de>
2025-01-29 15:30:21 +0100

    * adds modal tests

--- packages/src/components/modal/modal.test.tsx
+++ packages/src/components/modal/modal.test.tsx
@@ -1,45 +1,70 @@
-import {describe, expect, test} from 'vitest';
-import {render} from '@testing-library/react';
+import {beforeEach, describe, expect, type Mock, test, vi} from 'vitest';
+import {fireEvent, render, waitFor} from '@testing-library/react';
 import {Modal} from './modal';
 import React from 'react';
 
+let fn: Mock<(...args: any[]) => any>;
+
+beforeEach(() => {
+  fn = vi.fn();
+});
+
 describe('general', () => {
   test('should render notification', () => {
-    const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message']} confirmAction={() => {}} confirmLabel={'confirm label'}/>);
+    const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message']} confirmAction={fn} confirmLabel={'confirm label'}/>);
     
-    const header = container.querySelector('.header');
-    expect(header?.textContent).toEqual('Notify');
+    const header = container.getElementsByClassName('header');
+    expect(header[0].textContent).toEqual('Notify');
     
-    const content = container.querySelector('.content');
-    const text = content.querySelectorAll('p');
+    const content = container.getElementsByClassName('content');
+    expect(content[0]).toBeDefined();
+    
+    const text = content[0].getElementsByClassName('modalText');
 
     expect(text.length).toEqual(1);
-    expect(text[0]?.textContent).toEqual('test message');
+    expect(text[0].textContent).toEqual('test message');
     
-    const buttonWrapper = content.querySelector('.buttonWrapper');
-    expect(buttonWrapper?.children.length).toEqual(1);
+    const buttonWrapper = content[0].getElementsByClassName('buttonWrapper');
+    expect(buttonWrapper[0].children.length).toEqual(1);
     
-    const button = buttonWrapper.firstChild as HTMLButtonElement;
-    expect(button.textContent).toEqual('confirm label');
+    const button = buttonWrapper[0].firstChild;
+    expect(button?.textContent).toEqual('confirm label');
   });
   
   test('should render notification with multiple messages', () => {
     const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message', 'testing', 'foo']} confirmAction={() => {}} confirmLabel={'confirm label'}/>);
 
-    const content = container.querySelector('.content');
-    const text = content.querySelectorAll('p');
+    const content = container.getElementsByClassName('content');
+    expect(content).toBeDefined();
+    
+    const text = content[0].getElementsByTagName('p');
 
     expect(text.length).toEqual(3);
-    expect(text[0]?.textContent).toEqual('test message');
-    expect(text[1]?.textContent).toEqual('testing');
-    expect(text[2]?.textContent).toEqual('foo');
+    expect(text[0].textContent).toEqual('test message');
+    expect(text[1].textContent).toEqual('testing');
+    expect(text[2].textContent).toEqual('foo');
+  });
+
+  test('should execute confirm action', () => {
+    const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message']} confirmAction={fn} confirmLabel={'confirm label'}/>);
+
+    const buttonWrapper = container.getElementsByClassName('buttonWrapper').item(0) as Element;
+    const button = buttonWrapper.getElementsByTagName('button').item(0) as Element;
+
+    fireEvent.click(button);
+    expect(fn).toHaveBeenCalled();
   });
   
-  test('should render notification with timeout', () => {});
+  test('should render notification with timeout', () => {
+    const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message']} confirmAction={fn} confirmLabel={'confirm label'} timeout={500}/>);
+    
+    const progressWrapper = container.getElementsByClassName('progressWrapper');
+    expect(progressWrapper).toBeDefined();
+    
+    waitFor(() => {expect(fn).toHaveBeenCalled()}, {timeout: 500});
+  });
   
   test('should render question', () => {});
-  
-  test('should execute confirm action', () => {});
   
   test('should execute confirm action after timeout', () => {});
   
