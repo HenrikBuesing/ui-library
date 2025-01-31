78f96432d051ff8654f719c526db76cabeb6921b
HBuesing <hbuesing@newyorker.de>
2025-01-31 09:13:51 +0100

    * adds missing modal tests

--- packages/src/components/modal/modal.test.tsx
+++ packages/src/components/modal/modal.test.tsx
@@ -1,12 +1,14 @@
 import {beforeEach, describe, expect, type Mock, test, vi} from 'vitest';
-import {fireEvent, render, waitFor} from '@testing-library/react';
+import {fireEvent, render} from '@testing-library/react';
 import {Modal} from './modal';
 import React from 'react';
 
 let fn: Mock<(...args: any[]) => any>;
+let fnC: Mock<(...args: any[]) => any>;
 
 beforeEach(() => {
   fn = vi.fn();
+  fnC = vi.fn();
 });
 
 describe('general', () => {
@@ -45,6 +47,20 @@ describe('general', () => {
     expect(text[2].textContent).toEqual('foo');
   });
 
+  test('should render theme', () => {
+    const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message']} confirmAction={fn} confirmLabel={'confirm'} theme={'error'} dark={true}/>);
+    
+    const header = container.getElementsByClassName('header');
+    expect(header[0].className).to.include('error');
+  });
+  
+  test('should render dark mode', () => {
+    const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message']} confirmAction={fn} confirmLabel={'confirm'} theme={'error'} dark={true}/>);
+
+    const modal = container.getElementsByClassName('modal');
+    expect(modal[0].className).to.include('dark');
+  });
+
   test('should execute confirm action', () => {
     const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message']} confirmAction={fn} confirmLabel={'confirm label'}/>);
 
@@ -54,25 +70,62 @@ describe('general', () => {
     fireEvent.click(button);
     expect(fn).toHaveBeenCalled();
   });
+
+  test('should not execute confirm action (undefined func)', () => {
+    // @ts-ignore -> test undefined function
+    const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message']} confirmAction={undefined} confirmLabel={'confirm label'}/>);
+
+    const buttonWrapper = container.getElementsByClassName('buttonWrapper').item(0) as Element;
+    const button = buttonWrapper.getElementsByTagName('button').item(0) as Element;
+
+    fireEvent.click(button);
+    expect(fn).not.toHaveBeenCalled();
+  });
   
   test('should render notification with timeout', () => {
+    vi.useFakeTimers();
     const {container} = render(<Modal variant={'notification'} title={'Notify'} message={['test message']} confirmAction={fn} confirmLabel={'confirm label'} timeout={500}/>);
     
     const progressWrapper = container.getElementsByClassName('progressWrapper');
     expect(progressWrapper).toBeDefined();
     
-    waitFor(() => {expect(fn).toHaveBeenCalled()}, {timeout: 500});
+    vi.advanceTimersByTime(100);
+    expect(fn).not.toHaveBeenCalled();
+
+    vi.advanceTimersByTime(400);
+    expect(fn).toHaveBeenCalled()
   });
   
-  test('should render question', () => {});
-  
-  test('should execute confirm action after timeout', () => {});
-  
-  test('should execute cancel action', () => {});
+  test('should render question', () => {
+    const {container} = render(<Modal variant={'question'} title={'Notify'} message={['test message']} confirmAction={fn} confirmLabel={'confirm'} cancelLabel={'cancel'} cancelAction={fnC}/>);
 
-  test('should render custom content', () => {});
+    const buttonWrapper = container.getElementsByClassName('buttonWrapper');
+    expect(buttonWrapper[0].children.length).toEqual(2);
+
+    const confirm = buttonWrapper[0].firstChild;
+    expect(confirm?.textContent).toEqual('confirm');
+
+    const cancel = buttonWrapper[0].children[1];
+    expect(cancel?.textContent).toEqual('cancel');
+  });
   
-  test('should render custom content and timeout', () => {});
-  
-  test('should execute confirm action (custom content and timeout)', () => {});
+  test('should execute cancel action', () => {
+    const {container} = render(<Modal variant={'question'} title={'Notify'} message={['test message']} confirmAction={fn} confirmLabel={'confirm'} cancelLabel={'cancel'} cancelAction={fnC}/>);
+    const buttonWrapper = container.getElementsByClassName('buttonWrapper');
+    const cancel = buttonWrapper[0].children[1];
+    
+    fireEvent.click(cancel);
+    expect(fnC).toHaveBeenCalled();
+  });
+
+  test('should render custom content', () => {
+    const {container} = render(<Modal title={'custom content'}><div>Hello world</div></Modal>);
+    const content = container.getElementsByClassName('content');
+    
+    expect(content[0]).toBeDefined();
+    expect(content[0].firstChild?.textContent).toEqual('Hello world');
+
+    const buttonWrapper = content[0].getElementsByClassName('buttonWrapper');
+    expect(buttonWrapper[0]).not.toBeDefined();
+  });
 });
