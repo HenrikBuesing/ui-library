5869e4adc4690d6e557ee2e9992812274d9b13cf
HBuesing <hbuesing@newyorker.de>
2025-02-13 10:18:46 +0100

    * replaces date.now with performance.now to increase precision

--- packages/src/utils/generateKey.ts
+++ packages/src/utils/generateKey.ts
@@ -1,5 +1,5 @@
 export default function generateKey() {
   const m = Math.floor((Math.random() * 100) + 1);
 
-  return `uil-${(Date.now() * m).toString(32)}-${(Math.random() * m).toString(16).replace(/\./g, '')}`;
+  return `uil-${(performance.now() * m).toString(32)}-${(Math.random() * m).toString(16).replace(/\./g, '')}`;
 }
