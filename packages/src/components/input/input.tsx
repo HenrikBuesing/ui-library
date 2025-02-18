cc6c356de71c6bbfd48ca61bef5dfce010b0a519
HBuesing <hbuesing@newyorker.de>
2025-02-18 15:25:27 +0100

    * adds support for error prop

--- packages/src/components/input/input.tsx
+++ packages/src/components/input/input.tsx
@@ -8,6 +8,7 @@ import React from 'react';
 export function Input(props: Omit<InputProps, 'placeholder'>) {
   const {
     children,
+    error,
     helpText,
     id,
     label,
@@ -16,32 +17,36 @@ export function Input(props: Omit<InputProps, 'placeholder'>) {
   } = props;
 
   const ID = id ?? generateKey();
+  const helpId = helpText ? generateKey() : undefined;
   // const {theme} = useTheme(); ${theme === 'dark' ? ` ${global.dark}` : ''} preparation for darkmode refactor
 
   return (
     <div className={`${style.inputField}`}>
-      <div className={`${style.inputWrapper}${variant == 'basic' ? ` ${style.basic}` : ` ${style.outlined}`}`}>
-        <input 
+      <div className={`${style.inputWrapper}${variant == 'basic' ? ` ${style.basic}` : ` ${style.outlined}`}${error ? ` ${style.error}` : ''}`}>
+        <input
           id={ID}
           className={`${style.input} ${global.fontMedium}`}
           placeholder={''}
           {...inputProps}
+          aria-describedby={helpId}
         />
 
         {children && children}
-        
-        <fieldset className={style.fieldset} aria-hidden={true}>
+
+        <fieldset className={style.fieldset} aria-hidden>
           <legend className={style.legend}>
             <span className={style.labelText}>{label}</span>
+            <span className={`${style.labelText} ${style.asterisk}`} aria-hidden>*</span>
           </legend>
         </fieldset>
 
         <label htmlFor={ID} className={style.label}>
           <span className={style.labelText}>{label}</span>
+          <span className={`${style.labelText} ${style.asterisk}`} aria-hidden>*</span>
         </label>
       </div>
 
-      {helpText && <div className={style.helper}>{helpText}</div>}
+      {helpText && <div className={`${style.helpText}${error ? ` ${style.error}` : ''}`} id={helpId}>{helpText}</div>}
     </div>
   );
 }
