677713c6abf380603544bdbbda51b3bf34db0ca1
HBuesing <hbuesing@newyorker.de>
2025-03-04 11:06:50 +0100

    * replaces _meta files with a single _meta.global file

---
+++ docs/app/_meta.global.ts
@@ -0,0 +1,62 @@
+import {Meta} from "nextra";
+
+const globalMeta: Meta = {
+  index: {
+    type: 'page',
+    display: 'hidden',
+    theme: {
+      layout: 'full',
+      toc: false,
+      timestamp: false
+    }
+  },
+  docs: {
+    type: 'page',
+    title: 'Documentation',
+    items: {
+      components: {
+        title: 'Components',
+        theme: {
+          collapsed: true,
+        },
+        items: {
+          '-': {
+            type: 'separator',
+            title: 'Inputs'
+          },
+          button: 'Button',
+          input: 'Input',
+          checkbox: 'Checkbox',
+          radio: 'Radio',
+          '--': {
+            type: 'separator',
+            title: 'Data Display'
+          },
+          icon: 'Icon',
+          modal: 'Modal',
+        }
+      },
+      hooks: {
+        title: 'Hooks',
+        theme: {
+          collapsed: true
+        },
+        items: {
+          'contrast-color': 'useContrastColor'
+        }
+      }
+    }
+  },
+  about: {
+    type: 'page',
+    title: 'About',
+    theme: {
+      typesetting: 'article',
+      layout: 'full',
+      toc: false,
+      timestamp: false
+    }
+  }
+}
+
+export default globalMeta;
--- docs/app/docs/hooks/_meta.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  'contrast-color': 'useContrastColor'
-}
