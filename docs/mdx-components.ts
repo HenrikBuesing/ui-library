3ad3bc2bb1dbd0b6a432190bc2a796cfd30dd141
HBuesing <hbuesing@newyorker.de>
2025-03-04 11:05:58 +0100

    * removes unused code

--- docs/mdx-components.ts
+++ docs/mdx-components.ts
@@ -1,13 +1,1 @@
-// import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs';
-//
-// const themeComponents = getThemeComponents();
-//
-// // Merge components
-// export function useMDXComponents(components) {
-//   return {
-//     ...themeComponents,
-//     ...components
-//   };
-// }
-
 export { useMDXComponents } from 'nextra-theme-docs';
