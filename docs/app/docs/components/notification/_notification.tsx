4c31e5dc55ca6dd23db353ae82cabd47cbec4f14
HBuesing <buesing.henrik@t-online.de>
2025-04-08 14:29:43 +0200

    adds notification documentation

--- docs/app/docs/components/notification/_notification.tsx
+++ docs/app/docs/components/notification/_notification.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import {Notification} from '@hbuesing/ui-library';
+import {Button, Notification} from '@hbuesing/ui-library';
 import styles from '@/styles/styles.module.scss';
 import {useTheme} from "nextra-theme-docs";
 
@@ -9,37 +9,89 @@ export default function DefaultNotification() {
   const dark = theme === 'dark';
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+    <div className={styles.showcaseWrapperCol}>
       <Notification variant={'filled'} type={'info'} dark={dark}>
         Notification message
       </Notification>
+    </div>
+  );
+}
+
+export function NotificationVariant() {
+  const {theme} = useTheme();
+  const dark = theme === 'dark';
+
+  return (
+    <div className={styles.showcaseWrapperCol}>
+      <Notification variant={'filled'} type={'info'} dark={dark}>
+        Notification with variant <q>filled</q>
+      </Notification>
 
       <Notification variant={'outlined'} type={'info'} dark={dark}>
+        Notification with variant <q>outlined</q>
+      </Notification>
+    </div>
+  );
+}
+
+export function NotificationTypes({type}: {type: 'info' | 'success' | 'warning' | 'error'}) {
+  const {theme} = useTheme();
+  const dark = theme === 'dark';
+
+  return (
+    <div className={styles.showcaseWrapperCol}>
+      <Notification variant={'filled'} type={type} dark={dark}>
         Notification message
       </Notification>
 
-      <Notification variant={'filled'} type={'success'} dark={dark}>
+      <Notification variant={'outlined'} type={type} dark={dark}>
         Notification message
       </Notification>
+    </div>
+  );
+}
 
-      <Notification variant={'outlined'} type={'success'} dark={dark}>
-        Notification message
+export function NotificationCancel() {
+  const {theme} = useTheme();
+  const dark = theme === 'dark';
+
+  return (
+    <div className={styles.showcaseWrapperCol}>
+      <Notification variant={'filled'} type={'info'} dark={dark} onCancel={() => {alert('close button clicked')}}>
+        Notification with the predefined close button
       </Notification>
 
-      <Notification variant={'filled'} type={'warning'} dark={dark}>
-        Notification message
+      <Notification variant={'outlined'} type={'info'} dark={dark} onCancel={() => {alert('close button clicked')}}>
+        Notification with the predefined close button
+      </Notification>
+    </div>
+  );
+}
+
+export function NotificationAction() {
+  const {theme} = useTheme();
+  const dark = theme === 'dark';
+  
+  const color = dark? '#FFF' : '#000';
+
+  return (
+    <div className={styles.showcaseWrapperCol}>
+      <Notification
+        variant={'filled'}
+        type={'info'}
+        dark={dark}
+        action={<Button variant={'text'} size={'small'} color={'#FFF'} onClick={() => {alert('action clicked')}}>Close</Button>}
+      >
+        Notification with a custom close button
       </Notification>
 
-      <Notification variant={'outlined'} type={'warning'} dark={dark}>
-        Notification message
-      </Notification>
-
-      <Notification variant={'filled'} type={'error'} dark={dark}>
-        Notification message
-      </Notification>
-
-      <Notification variant={'outlined'} type={'error'} dark={dark}>
-        Notification message
+      <Notification
+        variant={'outlined'}
+        type={'info'}
+        dark={dark}
+        action={<Button variant={'text'} size={'small'} color={color} onClick={() => {alert('action clicked')}}>Close</Button>}
+      >
+        Notification with a custom close button
       </Notification>
     </div>
   );
