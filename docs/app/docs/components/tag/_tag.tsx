a5592e894b123aa51f83ad5d1dc96cf02a576321
HBuesing <buesing.henrik@t-online.de>
2025-05-22 13:41:15 +0200

    WIP tag docu

--- docs/app/docs/components/tag/_tag.tsx
+++ docs/app/docs/components/tag/_tag.tsx
@@ -3,11 +3,32 @@ import styles from '@/styles/styles.module.scss';
 import {Tag} from '@hbuesing/ui-library';
 import React from 'react';
 
+// <Tag variant={variant} label={'Large tag'} color={'#c322a7'} size={'large'}/>
+
 export default function Tags() {
   return (
     <div className={styles.showcaseWrapper}>
-      <Tag variant={'filled'} label={'Filled tag'} color={'#c322a7'}/>
-      <Tag variant={'outlined'} label={'Outlined tag'} color={'#c322a7'}/>
+      <Tag variant={'filled'} label={'Filled tag'} color={'info'}/>
+
+      <Tag variant={'outlined'} label={'Outlined tag'} color={'info'} onDelete={() => {alert('Outlined tag')}}/>
     </div>
-  )
+  );
+}
+
+export function TagSize() {
+  return (
+    <>
+      <div className={styles.showcaseWrapper}>
+        <Tag variant={'filled'} label={'Small tag'} color={'info'} size={'small'} onDelete={() => {alert('Outlined tag')}}/>
+        <Tag variant={'filled'} label={'Medium tag'} color={'info'} size={'medium'} onDelete={() => {alert('Outlined tag')}}/>
+        <Tag variant={'filled'} label={'Large tag'} color={'info'} size={'large'} onDelete={() => {alert('Outlined tag')}}/>
+      </div>
+
+      <div className={styles.showcaseWrapper}>
+        <Tag variant={'outlined'} label={'Small tag'} color={'info'} size={'small'}/>
+        <Tag variant={'outlined'} label={'Medium tag'} color={'info'} size={'medium'}/>
+        <Tag variant={'outlined'} label={'Large tag'} color={'info'} size={'large'}/>
+      </div>
+    </>
+  );
 }
