75eb8c0845af3cdae14a3f0397dd8e2b0feb006f
HBuesing <buesing.henrik@t-online.de>
2025-05-27 10:27:13 +0200

    adds new textarea component

--- packages/ui-library/src/index.ts
+++ packages/ui-library/src/index.ts
@@ -1,7 +1,7 @@
 export {Dialog, DialogTitle, DialogContent, DialogControls} from './components/dialog';
 export {ToastProvider, useToastContext, type ToastOptions} from './components/toast';
 export {RadioGroup, type RadioOption} from './components/check/radioGroup';
-export {Input, InputDecorator} from './components/input';
+export {Input, InputDecorator, Textarea} from './components/input';
 export {Checkbox} from './components/check/checkbox';
 export {Radio} from './components/check/radio';
 export {Button} from './components/button';
