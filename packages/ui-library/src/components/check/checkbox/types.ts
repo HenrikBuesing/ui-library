5f42b9fce05d7670bc51faf99861734342f81ef1
HBuesing <buesing.henrik@t-online.de>
2025-05-27 10:25:35 +0200

    simplifies type declaration

--- packages/ui-library/src/components/check/checkbox/types.ts
+++ packages/ui-library/src/components/check/checkbox/types.ts
@@ -1,9 +1,7 @@
 import type {BaseComponentProps} from '../../common/types';
 import type {CSSProperties, ReactNode} from 'react';
 
-export type CheckboxProps = BaseComponentProps<'input'> & AdditionalCheckboxProps;
-
-type AdditionalCheckboxProps = {
+export type CheckboxProps = BaseComponentProps<'input'> & {
   children?: ReactNode;
   color?: CSSProperties['backgroundColor'];
 };
