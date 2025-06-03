6ae0a6ca9117abcab639eafe01bd07fadcebc4da
HBuesing <buesing.henrik@t-online.de>
2025-06-03 08:53:25 +0200

    adds animation for determinate state & smaller optimisations

--- packages/ui-library/src/components/progress/circularProgress.tsx
+++ packages/ui-library/src/components/progress/circularProgress.tsx
@@ -1,41 +1,41 @@
 import global from '../common/styles/global.module.scss';
-import type {CircularProgressProps} from './types';
-import type {Status} from '../common/types';
 import styles from './progress.module.scss';
+import type {ProgressProps} from './types';
+import {isStatus} from '@utils/checkTypes';
 import cls from '@utils/conditionalClass';
 import React from 'react';
 
-export function CircularProgress(props: CircularProgressProps) {
+export function CircularProgress(props: ProgressProps) {
   const {
     color = 'info',
     dark,
-    size = 30,
+    size,
     value
   } = props;
   
-  if (value && (value > 100 || value < 0)) {
+  if (typeof value === 'number' && (value > 100 || value < 0)) {
     throw new Error(`invalid value. Expected 100 >= value >= 0, but got ${value}`);
   }
 
-  const strokeDasharray = value ? `${120 * (value / 100)}px, 120px` : 'initial';
-  const isCustomColor = (color !== 'info' && color !== 'success' && color !== 'warning' && color !== 'error');
+  const statusColor = isStatus(color);
 
   return (
     <div
       aria-valuenow={value}
-      className={cls([styles.circularProgress, !value && styles.indeterminate, dark && global.dark])}
+      className={cls([styles.circularProgress, typeof value !== 'number' && styles.indeterminate, dark && global.dark])}
       role={'progressbar'}
       style={{width: size, height: size}} 
     >
       <svg viewBox={`0 0 40 40`}>
         <circle 
-          className={cls([styles.spinner, !isCustomColor && styles[color as Status]])}
+          className={cls([styles.spinner, statusColor && styles[color]])}
           cx={20}
           cy={20}
           fill={'none'}
           r={19}
-          stroke={isCustomColor ? color : undefined}
-          strokeDasharray={strokeDasharray}
+          stroke={statusColor ? undefined : color}
+          strokeDasharray={typeof value === 'number' ? '120' : undefined}
+          strokeDashoffset={typeof value === 'number' ? `${1.2 * (100 - value)}px` : undefined}
           strokeWidth={2}
         />
       </svg>
