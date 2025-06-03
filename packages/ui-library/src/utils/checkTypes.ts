bc0d119ebd8193e6a00e57bcfa4ec4d65b6745f8
HBuesing <buesing.henrik@t-online.de>
2025-06-03 09:44:26 +0200

    removes unnecessary util func

--- packages/ui-library/src/utils/checkTypes.ts
+++ packages/ui-library/src/utils/checkTypes.ts
@@ -1,9 +1,5 @@
-import type {Size, Status} from '../components/common/types';
+import type {Status} from '../components/common/types';
 
 export function isStatus(value: unknown): value is Status {
   return typeof value === 'string' && ['info', 'success', 'warning', 'error'].includes(value);
-}
-
-export function isSize(value: unknown): value is Size {
-  return typeof value === 'string' && ['small', 'medium', 'large'].includes(value);
 }
--- packages/ui-library/src/utils/tests/checkTypes.test.ts
+++ packages/ui-library/src/utils/tests/checkTypes.test.ts
@@ -1,5 +1,5 @@
 import {describe, expect, test} from 'vitest';
-import {isSize, isStatus} from '@utils/checkTypes';
+import {isStatus} from '@utils/checkTypes';
 
 describe('isStatus', () => {
   test('should return valid status', () => {
@@ -11,22 +11,6 @@ describe('isStatus', () => {
   test('should return invalid status', () => {
     const invalidString = isStatus('testing');
     const nonString = isStatus(123);
-
-    expect(invalidString).toBeFalsy();
-    expect(nonString).toBeFalsy();
-  });
-});
-
-describe('isSize', () => {
-  test('should return valid Size', () => {
-    const test = isSize('large');
-
-    expect(test).toBeTruthy();
-  });
-
-  test('should return invalid Size', () => {
-    const invalidString = isSize('testing');
-    const nonString = isSize(123);
 
     expect(invalidString).toBeFalsy();
     expect(nonString).toBeFalsy();
