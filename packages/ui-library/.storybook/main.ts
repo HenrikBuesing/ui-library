d7638cf20a1f311fb64270b7ba3e3d06b0aae75f
HBuesing <buesing.henrik@t-online.de>
2025-06-18 11:36:23 +0200

    setup storybook

---
+++ packages/ui-library/.storybook/main.ts
@@ -0,0 +1,32 @@
+import type {StorybookConfig} from '@storybook/react-vite';
+import {join, dirname} from 'path';
+import * as path from 'node:path';
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
+
+const config: StorybookConfig = {
+  stories: [
+    '../**/*.stories.@(js|jsx|mjs|ts|tsx)'
+  ],
+  addons: [],
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {}
+  },
+  core: {
+    builder: '@storybook/builder-vite',
+    disableTelemetry: true
+  },
+  viteFinal: async (config) => {
+    config.resolve.alias = [
+      {find: '@common', replacement: path.resolve(__dirname, '../src/components/common/')},
+      {find: '@hooks', replacement: path.resolve(__dirname, '../src/hooks/')},
+      {find: '@utils', replacement: path.resolve(__dirname, '../src/utils/')}
+    ];
+    
+    return {...config};
+  }
+};
+export default config;
---
+++ packages/ui-library/.storybook/preview.ts
@@ -0,0 +1,16 @@
+import type { Preview } from '@storybook/react-vite'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+      disableSaveFromUI: true,
+    },
+    layout: 'centered',
+  },
+};
+
+export default preview;
