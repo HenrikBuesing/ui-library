cc5fe20749e581c6d787939b8179544fae2e9427
HBuesing <buesing.henrik@t-online.de>
2025-07-01 08:30:10 +0200

    adds textarea stories

--- packages/ui-library/src/components/input/textarea.stories.tsx
+++ packages/ui-library/src/components/input/textarea.stories.tsx
@@ -1,31 +1,50 @@
 import type {Meta, StoryObj} from '@storybook/react-vite';
+import {Textarea} from './textarea';
 import {fn} from 'storybook/test';
-import {Button} from '../button';
 
 const meta = {
-  title: 'Inputs/Text-Area',
-  component: Button,
+  title: 'Inputs/Textarea',
+  component: Textarea,
   argTypes: {
     dark: {control: 'boolean'},
-    rounded: {control: 'boolean'},
-    color: {control: 'select', options: ['info', 'success', 'warning', 'error', '#7f2bcf', '#39dddd']},
-    children: {control: 'text'},
+    error: {control: 'boolean'},
+    resize: {control: 'select', options: ['both', 'vertical', 'horizontal', 'none']},
+    rows: {control: 'number'},
+    cols: {control: 'number'},
   },
   args: {onClick: fn()},
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof Textarea>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Filled: Story = {
+export const Default: Story = {
   args: {
-    variant: 'filled',
-    children: 'Button',
+    label: 'Textarea',
     dark: false,
-    rounded: false,
-    size: 'medium'
+    rows: 1,
+    cols: 20,
+    helpText: '',
+    error: false
   },
   parameters: {
-    controls: {exclude: ['variant', 'dark']}
+    controls: {exclude: ['dark']}
+  }
+};
+
+export const Dark: Story = {
+  args: {
+    label: 'Textarea',
+    dark: true,
+    rows: 1,
+    cols: 20,
+    helpText: '',
+    error: false
+  },
+  parameters: {
+    controls: {exclude: ['dark']}
+  },
+  globals: {
+    backgrounds: {value: 'dark'}
   }
 };
